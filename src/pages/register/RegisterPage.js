551d5d6a4f1acbd2a30e04f15e2c49cb41376afd
Apipilikas <apipilikas@gmail.com>
2026-01-31 22:10:48 +0100

    Revert changes

--- src/pages/register/RegisterPage.js
+++ src/pages/register/RegisterPage.js
@@ -64,17 +64,16 @@ function Main() {
         if (!formRef.current.validate()) return;
         
         JudgeRequests.registerJudge(data).then(response => {
-            const config = DialogUtils.getInformDialogConfig("Activation email", JSON.stringify(response));
-            showDialog(config);
-            // if (response.success) {
-            //     const message = `Please visit your email [${data.email}] to activate your account.`;
-                
-            // }
-            // else {
-            //     const error = response.jsonData.error;
-            //     const config = new ErrorBoxConfig(error.description, "Contact Aggelos for further clarifications.", JSON.stringify(error.details), "ACTIVATION_ERROR");
-            //     showDialog(config);
-            // }
+            if (response.success) {
+                const message = `Please visit your email [${data.email}] to activate your account.`;
+                const config = DialogUtils.getInformDialogConfig("Activation email", message);
+                showDialog(config);
+            }
+            else {
+                const error = response.jsonData.error;
+                const config = new ErrorBoxConfig(error.description, "Contact Aggelos for further clarifications.", JSON.stringify(error.details), "ACTIVATION_ERROR");
+                showDialog(config);
+            }
         })
     }
     
